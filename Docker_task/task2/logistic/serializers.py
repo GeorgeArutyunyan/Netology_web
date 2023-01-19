a545a04592a4f1f1bfd1a2eb95d7273b2f77c945
George Arutyunyan <arutunyan.george@gmail.com>
2023-01-19 21:16:57 +0300

    inital

---
+++ Docker_task/task2/logistic/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
+
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'adress', 'products', 'positions']
+
+    def create(self, validated_data):
+
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+        for item in positions:
+            StockProduct.objects.create(
+                stock=stock,
+                product=item.get('product'),
+                quantity=item.get('quantity'),
+                price=item.get('price')
+            )
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().update(instance, validated_data)
+
+        for item in positions:
+            StockProduct.ojbects.update_or_create(
+                product=item.get('product'),
+                quantity=item.get('quantity'),
+                defaults={'price': item.get['price'],
+                         'quantity': item.get['quantity']}
+            )
+        return stock
